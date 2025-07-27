d88186f776f4022fba5dc71703071cf5c9f40193
olgapalmtag <palmtag73@freenet.de>
2025-07-27 20:23:24 +0000

    Rechte für kub Änderung

--- terraform/environments/dev/init_scripts/cloud-init-k3s.sh
+++ terraform/environments/dev/init_scripts/cloud-init-k3s.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+set -e
 
-curl -sfL https://get.k3s.io | sh -
+echo "Installing K3s with kubeconfig write permissions..."
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode 644" sh -
+
+echo "K3s installation complete."
+
