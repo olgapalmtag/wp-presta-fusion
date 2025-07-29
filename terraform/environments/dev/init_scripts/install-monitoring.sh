9c4c13ec8038e5e2d4b84015150b629d292f2fd6
olgapalmtag <palmtag73@freenet.de>
2025-07-29 22:10:36 +0000

    Ngnix fix

--- terraform/environments/dev/init_scripts/install-monitoring.sh
+++ terraform/environments/dev/init_scripts/install-monitoring.sh
@@ -22,10 +22,14 @@ helm repo update
 kubectl get namespace monitoring >/dev/null 2>&1 || kubectl create namespace monitoring
 
 # Install or upgrade kube-prometheus-stack
-helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
-  --namespace monitoring \
-  --set grafana.adminPassword=prom-operator \
-  --set grafana.service.type=NodePort \
-  --set prometheus.service.type=NodePort \
-  --set alertmanager.service.type=NodePort \
-  --wait
+for i in {1..3}; do
+  echo "Installing kube-prometheus-stack (attempt $i)..."
+  helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
+    --namespace monitoring --create-namespace \
+    --set grafana.adminPassword=prom-operator \
+    --set grafana.service.type=NodePort \
+    --set prometheus.service.type=NodePort \
+    --set alertmanager.service.type=NodePort \
+  echo "Helm install failed. Retrying in 10 seconds..."
+  sleep 10
+done
