6d7fd5bb73400adf707523c5d48994010c737a2b
olgapalmtag <palmtag73@freenet.de>
2025-07-29 09:47:07 +0000

    Install monitoring fix

--- terraform/environments/dev/init_scripts/install-monitoring.sh
+++ terraform/environments/dev/init_scripts/install-monitoring.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 set -e
 
-# Installiere Helm
-curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 -o get-helm.sh
-chmod +x get-helm.sh
-./get-helm.sh
+# Set KUBECONFIG path for K3s
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-# Prometheus + Grafana über Helm installieren
+# Install Helm (only if not already installed)
+if ! command -v helm >/dev/null 2>&1; then
+  echo "Installing Helm..."
+  curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 -o get-helm.sh
+  chmod +x get-helm.sh
+  ./get-helm.sh
+else
+  echo "Helm is already installed."
+fi
+
+# Add Helm repo for kube-prometheus-stack
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-helm install monitoring prometheus-community/kube-prometheus-stack \
-  --namespace monitoring --create-namespace
+# Create namespace if not existing
+kubectl get namespace monitoring >/dev/null 2>&1 || kubectl create namespace monitoring
 
+# Install or upgrade kube-prometheus-stack
+helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
+  --namespace monitoring \
+  --set grafana.adminPassword=prom-operator \
+  --set grafana.service.type=NodePort \
+  --set prometheus.service.type=NodePort \
+  --set alertmanager.service.type=NodePort \
+  --wait
--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,37 +1,3 @@
-resource "aws_security_group" "alb" {
-  name        = "${var.project}-alb-sg"
-  description = "Allow HTTP access to ALB"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "HTTP from Internet"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS from Internet"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project}-alb-sg"
-  }
-}
-
 resource "aws_security_group" "instance" {
   name        = "${var.project}-instance-sg"
   description = "Allow HTTP and SSH access to EC2"
